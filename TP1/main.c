bf15e4da6c2873cfb1a72a57c64a9efb57a12d9d
quipildor <40811766+quipildor@users.noreply.github.com>
2018-08-28 22:20:53 -0300

    Add files via upload

---
+++ TP1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int opcion, num1, num2;
+
+    do {
+        printf("Ingresar primer operando (A=%d)" , num1);
+        printf("Ingresar segundo operando (B=%d)" , num2);
+
+
+    } while ();
+
+
+    return 0;
+}
